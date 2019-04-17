2710bb4d98720b0cb58f9fe1136cec0e168963e1
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-04-17 17:33:49 -0500

    Final tests passed

--- StudentMinMax/Program.cs
+++ StudentMinMax/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace StudentMinMax
 {
@@ -6,12 +9,32 @@ namespace StudentMinMax
     {
         public static void Main()
         {
-            foreach (var student in students)
+            
+            string filePath = @"C:\Users\mjlan\source\repos\LotusMel\CoderGirl-StudentMinMax\studentdata.txt";
+            List<Student> studentList = new List<Student>();
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            Student student = new Student();
+
+            foreach (string line in lines)
             {
+                string[] entries = line.Split(' ');
+                int[] scores = new int[entries.Length - 1];
+                student.Name = entries[0];
+                for (int i = 1; i < entries.Length; i++)
+                {
+                    scores[i - 1] = int.Parse(entries[i]);
+                }
+                student.Scores = scores;
                 Console.WriteLine($"{student.Name} Min: {student.GetMinimumScore()} Max: {student.GetMaximumScore()}");
             }
 
             Console.ReadLine();
+                
+
         }
+
+        
     }
 }
--- StudentMinMax/Student.cs
+++ StudentMinMax/Student.cs
@@ -1,17 +1,31 @@
-﻿namespace StudentMinMax
+﻿using System.Linq;
+using System.IO;
+
+namespace StudentMinMax
 {
     public class Student
     {
         public string Name { get; set; }
 
         public int[] Scores { get; set; }
+        
 
-        public int? GetMaximumScore()
+        public int GetMaximumScore()
         {
+            
+
+            int maxScore = Scores.Max();
+              return maxScore;
         }
 
-        public int? GetMinimumScore()
+        public int GetMinimumScore()
         {
+            
+            
+            int minScore = Scores.Min();
+           return minScore;
         }
     }
+
+    
 }
